69aa18607121e33a6164ad026cf7dfe99a602565
Harry Miller <Harry.Miller@rrd.com>
2022-02-06 11:10:43 -0600

    Upgrade to .Net 6

--- Clean.ps1
+++ Clean.ps1
@@ -1,2 +1,2 @@
-cd "C:\Development\FileSystemProcessor"
-dir .\ -include bin,obj,_resharper*,packages,'.vs',TestResults -recurse | foreach($_) { rd $_.fullname –Recurse –Force}
+cd "C:\Development\EM\FileSystemProcessor"
+Get-ChildItem -include bin,obj,packages,'.vs',TestResults -Force -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse -ErrorAction SilentlyContinue -Verbose}
