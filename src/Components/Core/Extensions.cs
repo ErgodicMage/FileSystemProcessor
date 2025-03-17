925b6d3b862689897951dbdb76bdf71eab60afc1
ErgodicMage <ErgodicMage@gmail.com>
2025-03-16 20:56:43 -0500

    Added SafeDelete to Extensions for both FileInfo and DirectoryInfo

--- src/Components/Core/Extensions.cs
+++ src/Components/Core/Extensions.cs
@@ -5,5 +5,30 @@ public static class Extensions
     public static bool IsFile(this FileSystemInfo info) => (info.Attributes & FileAttributes.Normal) != 0;
 
     public static bool IsDirectory(this FileSystemInfo info) => (info.Attributes & FileAttributes.Directory) != 0;
+    
+    public static bool SafeDelete(this FileInfo fi)
+    {
+        try
+        {
+            fi.Delete();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 
+    public static bool SafeDelete(this DirectoryInfo di)
+    {
+        try
+        {
+            di.Delete();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 }
