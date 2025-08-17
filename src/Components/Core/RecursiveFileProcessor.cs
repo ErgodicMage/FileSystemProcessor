bdad6ee89e0a0348df72e59a24c433c74af11ea9
ErgodicMage <ErgodicMage@gmail.com>
2025-08-17 14:51:33 -0500

    Fixed issue with RecursiveFileProcessor.WithFolderOptions

--- src/Components/Core/RecursiveFileProcessor.cs
+++ src/Components/Core/RecursiveFileProcessor.cs
@@ -49,7 +49,7 @@ public class RecursiveFileProcessor : IRecursiveFileProcessor
 
     public IRecursiveFileProcessor WithFolderOptions(FindFilesOptions options)
     {
-        FolderOptions = FileOptions;
+        FolderOptions = options;
         return this;
     }
 
